ee0919456465b42cff95206fc75c3e88e4c39998
Ben <38230235+KBenster@users.noreply.github.com>
2025-09-21 13:35:23 -0700

    organize

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -24,6 +24,7 @@ import type {
 export * from '@stellar/stellar-sdk'
 export * as contract from '@stellar/stellar-sdk/contract'
 export * as rpc from '@stellar/stellar-sdk/rpc'
+import { Positions } from '@blend-capital/blend-sdk'
 
 if (typeof window !== 'undefined') {
   //@ts-ignore Buffer exists
